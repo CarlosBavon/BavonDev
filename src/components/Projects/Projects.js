7f349a3b49158819def5494fba802f560226494a
CarlosBavon <carlosbavon46@gmail.com>
2026-06-14 22:34:51 +0300

    Adding photo to admin proj

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,6 @@ import './Projects.css';
 import porkyImg from '../../assets/images/pork.png';
 import bavImg from '../../assets/images/bav.png'
 import hairImg from '../../assets/images/hair.png'
-import api from '../../utils/api';
 
 const allProjects = [
     {
@@ -94,17 +93,13 @@ const Projects = () => {
                             <div className="project-card__visual">
                                 {project.imageUrl ? (
                                     <img
-                                        src={
-                                            project.imageUrl.startsWith('http')
-                                                ? project.imageUrl
-                                                : `${api.defaults.baseURL}${project.imageUrl}`
-                                        }
+                                        src={project.imageUrl}
                                         alt={project.title}
                                         className="project-card__image"
                                     />
                                 ) : (
                                     <div className="project-card__placeholder">
-                                        <span className="project-card__placeholder-text">{project.title}</span>
+                                        <span>{project.title}</span>
                                     </div>
                                 )}
                             </div>
